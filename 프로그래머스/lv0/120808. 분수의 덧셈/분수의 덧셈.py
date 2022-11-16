88567ce6a63b500e7594897b2905879cd3d152ba
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-11-17 01:22:58 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.70 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -2,12 +2,12 @@ def solution(denum1, num1, denum2, num2):
     answer = [0, 0]
     answer[0] = (denum1 * num2) + (denum2 * num1)
     answer[1] = num2 * num1
-    max_value = 0
-    for a in range(1, answer[0] + 1):
+    ori = range(1, answer[0] + 1)
+    rev_ori=ori[::-1]
+    for a in rev_ori:
         if answer[0] % a == 0 and answer[1] % a == 0:
-            max_value = a
+            answer[0] = int(answer[0] / a)
+            answer[1] = int(answer[1] / a)
 
-    answer[0] = int(answer[0] / max_value)
-    answer[1] = int(answer[1] / max_value)
-
+            
     return answer
