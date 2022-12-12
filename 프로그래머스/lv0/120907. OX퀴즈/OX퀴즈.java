20fcb05f708ec4039ad4ea9b8902fd0ef22ceb5b
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-12 16:41:02 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -6,12 +6,12 @@ class Solution {
 
         for(int i=0;i<quiz.length;i++ ){
             String[] new_quiz=quiz[i].split(" ");
-             int a = Integer.parseInt(new_quiz[0]);
-             int b = Integer.parseInt(new_quiz[2]);
-             int c = Integer.parseInt(new_quiz[4]);
+             int a = Integer.parseInt(new_quiz[0]);   //X값
+             int b = Integer.parseInt(new_quiz[2]);    //Y값
+             int c = Integer.parseInt(new_quiz[4]);     //Z값 결과
 
             int result;
-            if(new_quiz[1].equals("+")){
+            if(new_quiz[1].equals("+")){        ///1번인덱스가 연산자
                 result = a + b;
             }else {
                 result = a - b;
