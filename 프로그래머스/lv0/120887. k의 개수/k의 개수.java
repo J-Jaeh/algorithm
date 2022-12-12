e13715978a05f345808c27bb8ba18198ed63a71c
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-12 16:57:24 +0900

    [level 0] Title: k의 개수, Time: 51.79 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,15 +1,10 @@
-public class TestEX {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.solution(3,10,2));
-    }
-}
 class Solution {
     public int solution(int i, int j, int k) {
         int count = 0;
         int startValue;
         int endValue = j;
         String replaceValue = "" + k;
+        
         for (startValue = i; startValue < endValue + 1; startValue++) {
             String stringValue = "" + startValue;
 
