9365304e7ac8aaa6e15358256b7a6c94d33f3a5e
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-20 09:50:52 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.10 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,6 +1,17 @@
 class Solution {
-    public String solution(String phone_number) {
-        String answer = "";
-        return answer;
-    }
+   public StringBuffer solution(String phone_number) {
+        int phone_number_length = phone_number.length();
+       
+        StringBuffer answer = new StringBuffer();
+       
+        String changeToStar = phone_number.substring(phone_number_length-4);
+       
+        for(int i=0;i<phone_number_length-4;i++){
+            answer.append("*");
+            
+        }
+      
+        return answer.append(changeToStar);
+}
+    
 }
