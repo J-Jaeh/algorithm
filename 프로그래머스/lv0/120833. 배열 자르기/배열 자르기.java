ba3a2ec1d11966f030bf8151b963ffcbaa97ece8
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-20 10:10:16 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] numbers, int num1, int num2) {
+        
+        ArrayList<Integer> newAnswerList = new ArrayList();
+        
+        for(int i =num1 ; i<num2+1;i++){
+            
+            newAnswerList.add(numbers[i]);
+        }
+        return newAnswerList;
+    }
+}
