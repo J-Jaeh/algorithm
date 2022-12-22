59157c1aa3f4db9e4c7a55d161209ddb859b34c8
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-23 01:04:16 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.16 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -18,7 +18,7 @@ class Solution {
 
         ArrayList<Integer> answers =new ArrayList<>(allNumber);
 
-        for (int i = 0; i < answers.size(); i++) {  //for each 돌릴라고 했는데 자료형이 불일치해서 실패..1
+        for (int i = 0; i < answers.size(); i++) { 
             answer += answers.get(i);
         }
         
