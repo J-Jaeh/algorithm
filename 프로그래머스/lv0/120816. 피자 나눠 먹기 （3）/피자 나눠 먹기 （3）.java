62560a61995cafd1ce1082634aaa4ff1c59938f9
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-22 09:40:56 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer =0;
+        
+        while(n>0){
+            n-=slice;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
+
