d33e5f9b449b5bbd9c005934e7fc99a446a64e0c
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-22 17:57:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.19 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer =0;
+        ArrayList<Integer> number = new ArrayList<>();
+        for (int i : numbers) {
+            number.add(i);
+        }
+
+        HashSet<Integer> allNumber = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        
+        HashSet<Integer> comparNumber = new HashSet<>(number);
+
+        allNumber.removeAll(comparNumber);
+
+        ArrayList<Integer> answers =new ArrayList<>(allNumber);
+
+        for (int i = 0; i < answers.size(); i++) {
+            answer += answers.get(i);
+        }
+        
+        return answer;
+    }
+}
