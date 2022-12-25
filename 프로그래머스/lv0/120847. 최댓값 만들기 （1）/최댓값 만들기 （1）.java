f772e5e4bd445c4ecee60a7232271c0db840886f
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-26 08:24:24 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.27 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Solution {
+   public int solution(int[] numbers) {
+        ArrayList<Integer> newNumbers = new ArrayList<>();
+
+        for (int number : numbers) {
+            newNumbers.add(number);
+        }
+        newNumbers.sort(Comparator.naturalOrder());
+        
+        return newNumbers.get(newNumbers.size()-1)*newNumbers.get(newNumbers.size()-2); 
+        }
+}
