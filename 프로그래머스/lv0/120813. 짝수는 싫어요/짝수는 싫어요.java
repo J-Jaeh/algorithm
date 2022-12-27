47c4a0ba48d3149d57b9f66fbe64028d6e5b87dc
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2022-12-28 08:42:26 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Solution {
+ public ArrayList<Integer> solution(int n) {
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i =1 ; i<n+1;i+=2){
+            answer.add(i);
+        }
+      
+        return answer;
+    }
+}
