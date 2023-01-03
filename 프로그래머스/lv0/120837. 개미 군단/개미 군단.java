789ee790c61212c9452c263e7429120a04c53c34
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-01-03 14:37:38 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int hp) {
+        int[] damge ={5,3,1};
+        int count =0;
+        for(int a : damge){
+            count += hp/a;
+            hp-=a*(hp/a);
+        }
+        
+        return count;
+    }
+
+}
+
