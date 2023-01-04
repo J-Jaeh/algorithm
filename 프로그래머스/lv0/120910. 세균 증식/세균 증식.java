ec0a7a400246555d936f55169ca67feeaa00ce7a
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-01-04 15:04:32 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n, int t) {
         int count =0;
-        int compare = n;
+        
         while(count<t){
             n*=2;
             count++;
