aa3e77fa8f3beff63ea547d8e049a89745de0ea3
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-01-10 00:14:49 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,28 +1,26 @@
 class Solution {
     public int solution(int num) {
-
-        if(num==1){
-            return 0;
-        }
-
         int count = 0;
-
-        while (num>1){
-
-            if(num%2==0){
-                num/=2;
+        
+        long numLong = num;
+        
+        while (numLong>1){
+            
+            if(numLong%2==0){
+                numLong/=2;
             }else {
-                num*=3;
-                num++;
+                numLong *=3;
+                numLong ++;
             }
+            
             count++;
         }
-
+        
         if(count>500){
             return -1;
         }
         else{
-            return num;
+            return count;
         }
     }
 }
