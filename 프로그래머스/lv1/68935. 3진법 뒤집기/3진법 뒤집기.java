c23a9166642927967bdd4cf5633b388c1c5b2bd0
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-01-12 15:57:00 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,44 +1,6 @@
-import java.util.ArrayList;
-
 class Solution {
-     public int solution(int n) {
-
-
-    //3^0,3^1,3^2,3^3,
-    //0,0,2,1
-    //2*9 + 1*27
-
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-
-    //10진법을 어떻게 3진법으로 바꿈 ? 3으로 나눔 ?
-
-    while(n>0){
-        //45 -> 15 ->5 ->3,2
-        // 일단  0    0   2 //!!1 ///
-
-        //125 -> 123 ->41 ->39 ->13 -> 12 ->4 -> 3
-            //  2         2          1       1   //1
-
-        if(n%3==0){
-            arrayList.add(0);
-            n/=3;
-        } else if ((n-1)%3==0){
-            arrayList.add(1);
-            n = (n-1)/3;
-        }else if((n-2)%3==0){
-            arrayList.add(2);
-            n = (n-2)/3;
-        }
-        //이게 뒤집혀진 결과이고 ..!
-    }
+    public int solution(int n) {
         int answer = 0;
-        int tree = 1;
-    for(int i = arrayList.size()-1; i>=0;i--){
-        answer += arrayList.get(i)*tree;
-        tree*=3;
-    }
         return answer;
-
     }
 }
