865418f55640b3904acdb8ef7363252cea4d0ac6
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-01-13 10:10:25 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+        int[] ints = new int[n+1];  //if n=3 [0,0,0] 인덱스 넘버로 ... 판별해야함 ...
+        // 합성수는 4이상임
+        //
+        for(int i =1; i<ints.length ;i++){
+
+            if(ints[i]==0){   //여기서 합성수 로직을 걸어주면 참 좋겠는데 ;;; 추가로 4보다 작으면 바로 컷
+                if(합성수(i)) {
+                    count++;
+                   /* for (int j = i; j < ints.length; j = j + i) {
+                        ints[j] = 1;
+                    }*/
+                }
+            }
+        }
+        
+        return count;
+    }
+    public boolean 합성수 (int i){
+        int count =0;
+        for(int j =1;j<i+1;j++){
+            if(i%j==0){
+                count++;
+            }
+        }
+        return count >= 3;
+    }
+
+}
