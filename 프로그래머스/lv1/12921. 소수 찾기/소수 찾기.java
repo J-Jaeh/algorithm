d9429ccbe94067c45bd0cf637ca76a67164de028
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-01-14 01:27:00 +0900

    [level 1] Title: 소수 찾기, Time: 22.03 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -2,14 +2,14 @@ class Solution {
     public int solution(int n) {
         int count = 0;
         
-        int[] sosuNumber = new int[n+1]; // 인덱스로 소수찾는다고 했는데
+        int[] sosuNumber = new int[n+1]; //[0,0,,,,,,,,,,,0] //인덱스로 소수찾다고 했는데
 
         for (int i =2 ; i<sosuNumber.length;i++) {
             if(sosuNumber[i]==0){
                 count++;
-                for(int j = i ; j<sosuNumber.length;j+=i ){
+                for(int j = i ; j<sosuNumber.length;j+=i ){//[2,4,6,8....]
                     sosuNumber[j]=-1;
-                }
+                }  //에레채 !
             }
         }
         return count;
