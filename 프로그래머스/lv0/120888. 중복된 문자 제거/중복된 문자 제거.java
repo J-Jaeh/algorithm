c638bb90b2c52d8e34f6a57e37393e70e9589a14
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-01-15 11:51:54 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.37 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,24 +1,42 @@
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.LinkedHashSet;
 
 class Solution {
- public StringBuffer solution(String my_string) {
-         StringBuffer answer = new StringBuffer();
+  public StringBuffer solution(String my_string) {
+//          StringBuffer answer = new StringBuffer();
         
+//         char[] new_my_string = my_string.toCharArray();
+        
+//         Set<Character> saveChar = new HashSet<>();
+        
+//         saveChar.add((char)(-1));
+        
+//         for (char c : new_my_string) {  
+//             if (!saveChar.contains(c)){
+//                 saveChar.add(c);
+//                 answer.append(c);
+//             }
+//         }  // 막상 해보니까 set으로 할 필요는 없었다.... set이용할라면 순서지킨 링크드 이용하면될듯...
+//         return answer;  
+      
+        StringBuffer answer = new StringBuffer();
+
         char[] new_my_string = my_string.toCharArray();
-        
-        Set<Character> saveChar = new HashSet<>();
-        
-        saveChar.add((char)(-1));
-        
-        for (char c : new_my_string) {  
-            if (!saveChar.contains(c)){
-                saveChar.add(c);
-                answer.append(c);
-            }
+
+        Set<Character> linkedSet = new LinkedHashSet<>();
+
+        for (char c : new_my_string) {
+            linkedSet.add(c);
+        }
+        for (Character character : linkedSet) {
+            answer.append(character);
         }
         return answer;
+      
     }
+    
+    
 
 }
