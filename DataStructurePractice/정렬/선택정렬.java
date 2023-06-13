20cd637fc32a5849a77b76feb93a52eda97259f3
J-Jaeh <1wo2gur@naver.com>
2023-06-14 08:37:09 +0900

    선택

---
+++ DataStructurePractice/정렬/선택정렬.java
@@ -0,0 +1,21 @@
+ import java.util.*;
+
+class Main{
+    public int[] solution(int n , int[] arr){
+        for(int i =0; i<n-1;i++){
+            int idx=i;
+            for(int j=i+1;k<n;j++){
+                if(arr[j]<arr[idx]) idx=j;  // 여기서 가장 작은값이 나온다... 오우..
+            }
+            int tmp = arr[i]; // i값을 일시적으로 저장
+            arr[i]=arr[idx] //찾은 최소값을 i값으로 바꿈
+            arr[idx]=tmp // 맞교환이라고 생각
+        }
+
+        return arr;
+
+    public void static main(String[] args){
+
+    }
+
+}
