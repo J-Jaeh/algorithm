142df223c44cf4435c7e42f96f4f3e9732a72d81
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-06-15 13:55:03 +0900

    삽입정렬 연습 오개념 수정.
    tmp값을 저장했으니까 배열에서 없어져도 된다

--- DataStructurePractice/정렬/연습.java
+++ DataStructurePractice/정렬/연습.java
@@ -8,12 +8,17 @@ class Main{
     
     for(int i = 1; i<n;i++){
       int tmp=arr[i],j;
-      for(int j =i-1;j>0;k--){
-        if(arr[j]>tmp){   //[ 5 ,2  ,1  ,4 ]-> [ 5 , k , 1 , 4]
-         arr[j]=arr[j+1] 
+      for(int j =i-1;j>0;k--){     // j>=0 까지 가야함 ! 이건 맞는말 같은데;
+        if(arr[j]>tmp){   //[ 5 ,2  ,1  ,4 ]-> [ 5 , 2 , 2 , 4] 사실 이게 되는거지?
+         arr[j+1]=arr[j]       // 여기선 arr[j+1]=arr[j]
+          // 이렇게 되고 ... 그니까 tmp는 기억해놨으니까 배열에도 없어져도 상관없자나
+          // 그러다가 에러만나면 브뤸크..
         }else break;
-        arr[j]=tmp;
+                   
       }
-      return arr;
+     arr[j+1]=tmp; // arr[j+1]=tmp를 넣네 .. ?  이유는 ... ? 없어진 tmp값이 들어갈 위치를 정하는것.
+    // j는 -1에서 끝나거나 break에서 끝나니까...? 그거보다 오른쪽 인덱스 값을 집어넣어야함..!
+     
   }
+   return arr;
 }
