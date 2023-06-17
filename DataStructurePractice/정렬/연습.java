7dbc53216fc1f13e4d31d4d664381f646f4fd08f
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2023-06-17 10:58:41 +0900

    Update 연습.java

--- DataStructurePractice/정렬/연습.java
+++ DataStructurePractice/정렬/연습.java
@@ -3,7 +3,7 @@
 
 class Main{
 
-  public int[] solution1_삽입(int a, int arr){
+  public int[] solution_삽입(int a, int[] arr){
    // 삽입의 기준...을 정해보면..  삼입되는 위치를 점점 늘려가는거겠지
     
     for(int i = 1; i<n;i++){
@@ -21,4 +21,25 @@ class Main{
      
   }
    return arr;
+  }
+ 
+   public int[] solution_선택(int n , int[] arr){
+      //선택 정렬이 되려면... 일단 기준하나를 잡고 j는 그것보다 크고...
+    for(int i =0;i<n-1;i++){
+      int temp = arr[i]; // 일단 최솟값을 저장하는거시여~.
+      int index; // 왜냐면 자리를 바꿀 j의 인덱스를 알아야하자나...
+     for(int j = i+1; j<n ; j++){
+        if(temp>arr[j]){
+          //작은값이 계속 업데이트가 되어야하자나
+          temp = arr[j];
+          index = j;
+        } 
+     }
+     //이렇게 한바퀴를 돌고나면 .. 가장작은값은 temp!
+     arr[index]=arr[i];
+     arr[i]=temp; 
+   }
+    return arr;
+  }
+ 
 }
