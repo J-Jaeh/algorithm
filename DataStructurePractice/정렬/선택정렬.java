f4efd39603285271ad6ccaa42fa2530f33b86cf4
J-Jaeh <1wo2gur@naver.com>
2023-06-19 08:35:06 +0900

    LRUI

--- DataStructurePractice/정렬/선택정렬.java
+++ DataStructurePractice/정렬/선택정렬.java
@@ -5,7 +5,7 @@ class Main{
         for(int i =0; i<n-1;i++){
             int idx=i;
             for(int j=i+1;k<n;j++){
-                if(arr[j]<arr[idx]) idx=j;  // 여기서 가장 작은값이 나온다... 오우..
+                if(arr[j]<arr[idx]) idx=j;  // 여기서 가장 작은값이 나온다... 아.. 작은값을 index값으로 ..
             }
             int tmp = arr[i]; // i값을 일시적으로 저장
             arr[i]=arr[idx] //찾은 최소값을 i값으로 바꿈
---
+++ KTW/LRU.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Main {
+    public int[] solution(int size;
+
+    int n, int[] arr)
+
+    {
+        int[] cache = new int[size];
+        for (int x : arr) {
+            int pos = -1; // 위치
+            for (int i = 0; i < size; i++) if (x == cache[i]) pos = i; //x값이 캐시에 있다면 있는 인덱스를 저장;;
+
+            if (pos == -1) {
+                for (int i = size - 1; i >= 1; i--) {
+                    cache[i] = cache[i - 1];
+                }
+
+            } else {
+                for (int i = pos; i >= 1; i--) { //히트인경우 .. 히트인 자리부터... 뒤로 밀리기..
+                    cache[i] = cache[i - 1];
+                }
+            }
+            cache[0] = x;// 한번만 해줘도 됨... ! 각각의 경우에 해주는것보단 .. 어차피 0번 인덱스에 들어가니까..
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(Systme.in);
+        int s = kb.nextInt();
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+
+        for (int x : T.solution(s, n, arr)) System.out.print(x + " ");
+
+    }
+}
