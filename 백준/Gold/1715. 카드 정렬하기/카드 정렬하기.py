9e7c071f7b9cc8d5e6b370d916bf5897bfbc929e
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-03-26 13:08:31 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 192 ms, Memory: 33972 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -8,10 +8,11 @@ N=int(input())
 
 # 우선 순위 큐에 답아서 더하고 넣고 .? 반복 언제지 빌때 까지 ?
 ## 한개일경우 바로 값리턴 2개일 경우부터 넣고 돌리기
-card_list=[int(input()) for _ in range(N)]
-h=[]
-for i in range(N):
-    heapq.heappush(h,card_list[i])
+h=[int(input()) for _ in range(N)]
+# h=[]
+heapq.heapify(h)
+# for i in range(N):
+#     heapq.heappush(h,card_list[i])
 # print(h)
 # print(len(h))
 answer=0
