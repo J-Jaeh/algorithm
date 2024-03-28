d6ee3a09667a111a95060a7fa226d8fbc2136d36
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-03-28 20:29:44 +0900

    [Silver III] Title: 1로 만들기, Time: 548 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,37 +1,23 @@
-## 역으로 바텀업 연습해보자
-# 1에서 시작 3곱하고 2곱하고 1곱하고
-## 방문배열만들어서 체크...
 import sys
-from collections import deque
-
 
 def input():
-    return sys.stdin.readline().rstrip()
-
-N=int(input())
-
-count=0
-Q=deque([(0,1)])
-visit=[True]*(N+1)
-# print(len(visit))
-while True:
-    #c = count m s=목표값.
-    c,s=Q.popleft()
-    count=c
-    # print(s)
-    if s==N:
-        break
-    else:
-        # print(s*3)
-        if s*3<=N and visit[s*3]:
-          visit[s * 3]=False
-          Q.append((c+1,s*3))
-        if s*2<=N and visit[s*2]:
-          visit[s * 2] = False
-          Q.append((c+1,s*2))
-        if s+1<=N and  visit[s+1]:
-          visit[s + 1] = False
-          Q.append((c + 1, s+1))
+    return sys.stdin.readline().strip()
 
 
-print(count)
+
+n=int(input())
+dp=[0]*(n+1)
+
+for i in range(2,n+1):
+    # 자연스래 증가하는 값.. 이것은
+    ## 1로 채워짐
+    dp[i] = dp[i-1]+1
+    ## 2로 나누어질때?
+    if i % 2 == 0:
+        dp[i] = min(dp[i],dp[i//2] + 1)
+    # 3으로 나누어질때.
+    if i % 3 ==0:
+        dp[i] = min(dp[i],dp[i//3] + 1)
+    # print(dp)
+
+print(dp[n])
