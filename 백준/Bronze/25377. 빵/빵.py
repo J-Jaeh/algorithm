002bd0eb1375f366727c48019f24364952e0df0f
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-03-28 10:09:42 +0900

    [Bronze IV] Title: 빵, Time: 44 ms, Memory: 31120 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.py
@@ -0,0 +1,21 @@
+#Qjd
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N=int(input())
+
+a_list=[]
+
+for _ in range(N):
+ A,B = map(int,input().split())
+ if A<B:
+    a_list.append(B)
+
+if not a_list:
+    print(-1)
+else:
+     a_list.sort()
+     print(a_list[0])
