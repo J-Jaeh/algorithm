ebfaac55015286e689a028dc000b87b8f9b0f6b6
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-03-30 20:24:07 +0900

    [Gold V] Title: 동전 2, Time: 240 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,48 +1,28 @@
-## 재귀 + for 문
-## bfs니까 ..... for 문이 아니라 넓이 우선 탐색 == 최솟값 !! ! ! !
+# 동전 22
+# 기본 아이디어는 동전이랑 유사
 import sys
-from collections import deque
 
 
 def input():
     return sys.stdin.readline().rstrip()
 
+def program():
+    coins, target = map(int, input().split())
 
-n ,k = map(int,input().split())
+    coin_list = [int(input()) for _ in range(coins)]
+    set_coin_list = set(coin_list)
 
-coin_list=[int(input()) for _ in range(n)]
-
-coin_list.sort()
-# print(coin_list)
-count=0
-#일단 큐에 다 담고 돌리기 보통의 생각은 이거겠지만
-## 목표값을 코인에 넣고 돌리기
-# 아 이런 방법이 있구나 그러면 동일 깊이는 같이 가는구나
-q=deque([(k,count)])
-
-duplicated_list=set()
-while q:
-    # print(q)
-    target,count = q.popleft()
-    count += 1
-    # 타겟을 계속 업데이트 시켜주면 안됨 그니까 for 한번 돌때는 같은 값이 들어가야함
-    for x in coin_list:
-        temp=target
-        temp-=x
-        if temp ==0:
-            print(count)
-            exit()
-        elif temp <0:
-            break
-        else:
-            if temp not in duplicated_list:
-                duplicated_list.add(temp)
-                q.append((temp,count))
-else:
-    print(-1)
-    exit(0)
+    coin_count = [1e9] * (target + 1)
+    coin_count[0]=0
+    
+    for i, coin in enumerate(set_coin_list):
+        for t in range(coin,target + 1):
+            # print(coin)
+            if t >= coin:
+                coin_count[t] = min(coin_count[t - coin]+1,coin_count[t])
+                # print(coin_count)
+    print(coin_count[-1] if coin_count[-1] != 1e9 else -1)
 
 
-
-
-
+if __name__ == "__main__":
+    program()
