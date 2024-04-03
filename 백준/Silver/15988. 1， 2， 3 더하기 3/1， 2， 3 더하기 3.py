d33c206372de4573e4dc1e2ff8d1dbb8e33cbccc
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-04-03 14:31:58 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 424 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,20 @@
+# 모르겠는데 공식찍기 실패~
+# 답지봄
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+dp=[0]*1000001
+dp[1]=1
+dp[2]=2
+dp[3]=4
+for i in range(4,1000001):
+    dp[i]=(dp[i-1]+dp[i-2]+dp[i-3])%1000000009
+
+
+if __name__ == '__main__':
+    N=int(input())
+    for _ in range(N):
+        n=int(input())
+        print(dp[n])
