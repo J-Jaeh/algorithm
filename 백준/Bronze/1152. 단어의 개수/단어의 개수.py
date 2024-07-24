13f804c43e6174aa6ff857e1a1f2224b4a3d982b
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-07-24 11:18:02 +0900

    [Bronze II] Title: 단어의 개수, Time: 80 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,1 +1,18 @@
-print(len(list(map(str,input().split()))))
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def sol(input:str)->int:
+    if not input:
+        return 0
+    count =1
+    for x in input:
+        if x == ' ':
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    input = input().strip()
+    print(sol(input))
