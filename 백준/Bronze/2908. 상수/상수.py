b12fba520c01bf2a44c1f43d140f00dc4751fbcf
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-07-27 00:20:54 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,7 +1,17 @@
-arr = list(map(str,input().split()))
+import sys
 
-v_1 =int(arr[0][::-1])
-v_2 =int(arr[1][::-1])
 
-big = v_1 if v_1>v_2 else v_2
-print(big)
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def sol(A:str,B:str)->int:
+    return max(int(A[::-1]),int(B[::-1]))
+
+if __name__ == '__main__':
+
+    A,B = map(str,input().split())
+
+    print(sol(A,B))
+
+
