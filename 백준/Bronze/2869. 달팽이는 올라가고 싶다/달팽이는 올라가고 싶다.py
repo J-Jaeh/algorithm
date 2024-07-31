a265c7674024b55f61bd66cae668e3675bb07ed4
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-07-31 23:28:32 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,12 +1,22 @@
 import sys
 
-arr=list(map(int,sys.stdin.readline().split()))
 
-A=arr[0]
-B=arr[1]
-V=arr[2]
+def input():
+    return sys.stdin.readline().rstrip()
 
-count_day=(V-A)//(A-B)
-remain_v=(V-A)%(A-B)
-if remain_v<A and remain_v !=0:count_day+=1
-print(count_day+1)
+
+def sol(A: int, B: int, V: int) -> int:
+    if A == V: return 1
+
+    new_v = V - A
+
+    # 내가 착각하고 있는거는 마지막날에 대한 처리가 정확하지 않았다는것... 그니가 첫날 은 생각해서 1더했는데 마지막날은
+    # B만큼 빼줄 필요가 없다는 뜻..?
+
+    day = (new_v+A-B -1) // (A - B)
+    return day+1
+
+
+if __name__ == "__main__":
+    a, b, v = map(int, input().split())
+    print(sol(a, b, v))
