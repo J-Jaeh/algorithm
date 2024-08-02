7d70ddba6a560b18b7f8c5afd35cca21b9072410
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-08-02 10:39:21 +0900

    [Silver IV] Title: 한수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,11 +1,24 @@
-n=int(input())
-hansu=0
-if n<100:
-    hansu=n
-else:
-    hansu=99
-    for i in range(100,n+1):
-        arr = list(str(i))
-        if int(arr[0])-int(arr[1]) == int(arr[1])-int(arr[2]):
-            hansu+=1
-print(hansu)
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def sol(x:int)->int:
+    if x <100:
+        return x
+    count = 99
+    for i in range(100, x+1):
+        if i <1000:
+            l = i//100
+            m = (i-l*100)//10
+            r = i-l*100-m*10
+            if l-m == m-r:
+                count +=1
+
+    return count
+
+
+if __name__=='__main__':
+    print(sol(int(input())))
