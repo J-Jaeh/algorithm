f930eb0a46696e6a7997bed7027a7ae174306e0e
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2024-08-15 23:25:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.53 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,48 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int solution(int[][] maps) {
-        int y_len = maps.length;
-        int x_len = maps[0].length;
-
-        int[] dx = {1,-1,0,0};
-        int[] dy = {0,0,-1,1};
-
+        int answer = 0;
+        int y_len=maps.length;
+        int x_len=maps[0].length;
         boolean[][] vi = new boolean[y_len][x_len];
-
-        Deque<Node> q = new ArrayDeque<>();
-        int dist = 1;
-
-        q.add(new Node(0,0,dist));
         vi[0][0]=true;
-        while (!q.isEmpty()){
-            Node node = q.pollFirst();
-            int o_x =node.x;
-            int o_y =node.y;
-            if (o_x == x_len-1 && o_y== y_len-1){
-                return node.dist;
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
+        Deque<Node> q = new ArrayDeque<>();
+        q.add(new Node(0,0,1));
+        while(!q.isEmpty()){
+           Node dot  = q.pollFirst();
+           int o_y = dot.y;
+           int o_x = dot.x;
+           
+           if (o_y==y_len-1 && o_x == x_len -1){
+                return dot.dist;
+           }
+           for(int i =0;i<4;i++){
+            int n_y = o_y + dy[i];
+            int n_x = o_x + dx[i];
+            
+            if (0<=n_y && 0<=n_x && n_y<y_len && n_x<x_len&&maps[n_y][n_x]==1&&!vi[n_y][n_x]){
+            vi[n_y][n_x]=true;
+            q.add(new Node(n_x,n_y,dot.dist+1));
             }
-
-                for(int i=0;i<4;i++){
-                    int n_x = o_x+dx[i];
-                    int n_y = o_y+dy[i];
-                    if(0<=n_y&&0<=n_x&&n_y<y_len&&n_x<x_len&&!vi[n_y][n_x]&&maps[n_y][n_x]==1){
-                        vi[n_y][n_x]=true;
-                        q.add(new Node(n_x,n_y,node.dist+1));
-                    }
-                }
+           }
+           
+           
+           
         }
-
+        
         return -1;
     }
-
+    
     private class Node{
         int x;
         int y;
         int dist;
-        public Node(int x, int y,int dist) {
-            this.x = x;
-            this.y = y;
+        public Node(int x, int y, int dist){
+            this.x=x;
+            this.y=y;
             this.dist=dist;
         }
     }
