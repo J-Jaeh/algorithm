597a0602d444fb488f7c4779329ed3cb18591511
J-Jaeh <117059721+J-Jaeh@users.noreply.github.com>
2025-01-30 10:10:53 +0900

    [Bronze I] Title: 이항 계수 1, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -8,13 +8,21 @@ public class Main {
         if (s == f || s==0) {
             System.out.println(1);
         } else {
-            System.out.println(factorial(f) / (factorial(s) * factorial(f - s)));
+//            System.out.println(factorial(f) / (factorial(s) * factorial(f - s)));
+            System.out.println(factorial(f,s));
         }
-        
+
     }
-    
-    private static int factorial(int n) {
-        if (n <= 1) return n;
-        return n * factorial(n - 1);
+
+    private static long factorial(int n,int k) {
+//        if (n <= 1) return n;
+//        return n * factorial(n - 1);
+        if(k>n-k) k = n-k;
+        long result =1;
+        for(int i =0;i<k;i++){
+            result*=(n-i);
+            result/=(1+i);
+        }
+        return result;
     }
 }
